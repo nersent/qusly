daf22f2367a10a54407124062790c7073db2bb90
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-05-19 21:32:14 +0200

    Prepare for refactor

---
+++ src/models/sftp-client.ts
@@ -0,0 +1,11 @@
+import { Client as SshClient, SFTPWrapper } from "ssh2";
+
+export class SFTPClient {
+  private _ssh: SshClient;
+
+  private _wrapper: SFTPWrapper;
+
+  public connect() {
+
+  }
+}
