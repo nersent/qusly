980d863ba805d11b465981d3cc852956aaa17b8e
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-09-27 21:19:58 +0200

    fix: change host property to optional in config interface

--- src/interfaces/config.ts
+++ src/interfaces/config.ts
@@ -2,7 +2,7 @@ export type IProtocol = 'sftp' | 'ftp' | 'ftps';
 
 export interface IConfig {
   protocol?: IProtocol;
-  host: string;
+  host?: string;
   port?: number;
   user?: string;
   password?: string;
