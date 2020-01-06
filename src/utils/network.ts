2c281c229e69ec4ac3aecb47db0cc13bb30038cf
Mikoaj Palkiewicz <xnerhu@gmail.com>
2020-01-06 16:57:04 +0100

    fix: calculating estimated time arrival

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -5,5 +5,8 @@ export const calcElapsed = (start: number) => {
 export const calcEta = (elapsed: number, buffered: number, size: number) => {
   const rate = buffered / elapsed;
   const estimated = size / rate;
-  return Math.round(estimated - elapsed);
+
+  const eta = Math.round(estimated - elapsed);
+
+  return Number.isSafeInteger(eta) ? eta : 1;
 }
